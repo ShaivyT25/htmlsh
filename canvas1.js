7ce9eb6e80643065c0a042987ce1ce120419698e
Shaivy Tiwari <shaivyt031@gmail.com>
2020-07-14 12:01:58 -0700

    Circle Animation

--- canvas1.js
+++ canvas1.js
@@ -27,3 +27,46 @@ c.beginPath();
 c.arc(300, 300, 30, 0, Math.PI * 2, false);
 c.strokeStyle = "blue";
 c.stroke();
+
+// for (let i = 0; i < 1; i++) {
+//     var x = Math.random() * window.innerHeight;
+//     var y = Math.random() * window.innerWidth;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = "blue";
+//     c.stroke();
+// }
+
+// bouncing ball
+
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random()-0.5) * 10;
+var dy = (Math.random()-0.5) * 10;  
+var radius = 30;
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0,0,innerWidth, innerHeight);  
+
+  c.beginPath();
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
+  c.fillStyle = "gold";
+  c.strokeStyle = "green";
+  c.fill();
+  c.stroke();
+  
+  // boundary condition 
+  // once touches boundry bounces back
+  // all directions keeps moving
+  if(x+radius > innerWidth || x-radius < 0){
+      dx = -dx;
+  }
+  
+  if(y+radius > innerHeight || y-radius < 0){
+    dy = -dy;
+}
+  x+=dx;
+  y+=dy
+
+}
+animate();
