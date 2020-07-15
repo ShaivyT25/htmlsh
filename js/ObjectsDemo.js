42e808d2823dec85689555b5483a63a03f67c227
Shaivy Tiwari <shaivyt031@gmail.com>
2020-07-15 13:32:53 -0700

    JavaScript basics

---
+++ js/ObjectsDemo.js
@@ -0,0 +1,14 @@
+// Object creation syntax
+var user = new Object(); 
+
+// Object literal syntax
+const user = {
+    name: "Will",
+    age: 22,
+    homeAddress: "Paris" 
+}; 
+
+user.name = "Bill";
+
+console.log(user.name);
+console.log(user["age"]);
